f62390ec2639372bd334de7e4f9f86c86adab15c
Jason Ross <jason@jasons-mac-mini.home>
2014-06-08 11:17:51 -0400

    Initial commit for sandbox

---
+++ SandBox/IMNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IMNAppDelegate.h
+//  SandBox
+//
+//  Created by Jason Ross on 6/8/14.
+//  Copyright (c) 2014 inMotion-Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SandBoxTests/SandBoxTests.m
@@ -0,0 +1,34 @@
+//
+//  SandBoxTests.m
+//  SandBoxTests
+//
+//  Created by Jason Ross on 6/8/14.
+//  Copyright (c) 2014 inMotiion-Networks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SandBoxTests : XCTestCase
+
+@end
+
+@implementation SandBoxTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
